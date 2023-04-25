40f4fd4cbe90163caeb4ab186a5e1c34ab0ea238
zroygbiv <109639906+zroygbiv@users.noreply.github.com>
2023-04-24 17:57:14 -0700

    Delete local-api.js

--- packages/local-client/local-api.js
+++
@@ -1,9 +0,0 @@
-// import express
-
-// one route - serve up react production assets
-
-// another route - save a list of cells into a file
-
-// load up a list of cells from a file
-
-// express server listen on port 4050
